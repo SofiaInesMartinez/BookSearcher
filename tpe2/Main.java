bf6686da1c231d14225541bd509c7a9d2f2204dd
Tomás Rodríguez <augustotomasrodriguez@gmail.com>
2022-06-28 12:54:00 -0300

    Probado con dataset 4.

--- tpe2/Main.java
+++ tpe2/Main.java
@@ -4,13 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		String dataset1 = new String("C:\\Users\\maxi_\\Documents\\Sofi\\PROGRAMACION_3\\TPE\\dataset1.csv");
-		String dataset2 = new String("C:\\Users\\maxi_\\Documents\\Sofi\\PROGRAMACION_3\\TPE\\dataset2.csv");
-		String dataset3 = new String("C:\\Users\\maxi_\\Documents\\Sofi\\PROGRAMACION_3\\TPE\\dataset3.csv");
+		String dataset1 = new String("C:\\Users\\Rodríguez\\OneDrive\\Documentos\\TUDAI\\Datasets2daEtapa\\dataset1.csv");
+		String dataset2 = new String("C:\\Users\\Rodríguez\\OneDrive\\Documentos\\TUDAI\\Datasets2daEtapa\\dataset2.csv");
+		String dataset3 = new String("C:\\Users\\Rodríguez\\OneDrive\\Documentos\\TUDAI\\Datasets2daEtapa\\dataset3.csv");
+		String dataset4 = new String("C:\\Users\\Rodríguez\\OneDrive\\Documentos\\TUDAI\\Datasets2daEtapa\\dataset4.csv");
 
 		GrafoDirigido grafo1 = crearGrafo(dataset1);
 		GrafoDirigido grafo2 = crearGrafo(dataset2);
 		GrafoDirigido grafo3 = crearGrafo(dataset3);
+		GrafoDirigido grafo4 = crearGrafo(dataset4);
 		System.out.println(grafo1);
 
 		GenerosMasBuscadosDesdeA top = new GenerosMasBuscadosDesdeA(grafo1);
@@ -20,24 +22,24 @@ public class Main {
 		SecuenciaAltoValor s = new SecuenciaAltoValor(grafo1);
 		System.out.println("Secuencia de mayor valor a partir de investigación " + s.obtenerSecuencia("investigación"));
 
-		GrafoDirigido grafo4 = new GrafoDirigido();
-		grafo4.agregarVertice("A");
-		grafo4.agregarVertice("B");
-		grafo4.agregarVertice("C");
-		grafo4.agregarVertice("D");
-		grafo4.agregarVertice("E");
-		grafo4.agregarVertice("F");
-		grafo4.agregarVertice("G");
-		grafo4.agregarArco("A", "B");
-		grafo4.agregarArco("A", "G");
-		grafo4.agregarArco("B", "E");
-		grafo4.agregarArco("B", "F");
-		grafo4.agregarArco("B", "C");
-		grafo4.agregarArco("C", "D");
-		grafo4.agregarArco("D", "B");
-		grafo4.agregarArco("E", "A");
+		GrafoDirigido grafo5 = new GrafoDirigido();
+		grafo5.agregarVertice("A");
+		grafo5.agregarVertice("B");
+		grafo5.agregarVertice("C");
+		grafo5.agregarVertice("D");
+		grafo5.agregarVertice("E");
+		grafo5.agregarVertice("F");
+		grafo5.agregarVertice("G");
+		grafo5.agregarArco("A", "B");
+		grafo5.agregarArco("A", "G");
+		grafo5.agregarArco("B", "E");
+		grafo5.agregarArco("B", "F");
+		grafo5.agregarArco("B", "C");
+		grafo5.agregarArco("C", "D");
+		grafo5.agregarArco("D", "B");
+		grafo5.agregarArco("E", "A");
 
-		GrafoGenerosVinculados vinculados = new GrafoGenerosVinculados(grafo2);
+		GrafoGenerosVinculados vinculados = new GrafoGenerosVinculados(grafo4);
 		System.out.println("solucion " + vinculados.getGenerosVinculados("investigación"));
 		
 
